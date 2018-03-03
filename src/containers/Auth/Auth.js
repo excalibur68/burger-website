c9e843ab41f8740b7839c1498330af59f4fbcb1f
Yan Cao <caoyan1994@g.ucla.edu>
2018-03-02 18:22:02 -0800

    before next

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 import {connect} from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class Auth extends Component {
     state={
@@ -39,6 +40,11 @@ class Auth extends Component {
             }
         },
         isSignup: true
+    }
+    componentDidMount(){
+       if( !this.props.buildingBurger && this.props.authRedirectPath !== '/'){
+            this.props.onSetAuthRedirectPath();
+       }
     }
     checkValidity(value, rules) {
         let isValid = true;
@@ -124,8 +130,14 @@ class Auth extends Component {
                 <p>{this.props.error.message}</p>
             );
         }
+
+        let authRedirect = null
+        if(this.props.isAuthenticated){
+            authRedirect = <Redirect to={this.props.authRedirectPath}/>
+        }
         return(
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -142,13 +154,17 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     };
 }
 
 const mapDispatchToProps = dispatch => {
     return{
-        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     };
 };
 
